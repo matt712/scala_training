13c0784ca9a5fdce2255c75a9ae6cf508c794396
matt712 <mzgadd@gmail.com>
2019-07-11 09:01:42 +0100

    day 2 work

--- src/main/scala/day2/DayTwo.scala
+++ src/main/scala/day2/DayTwo.scala
@@ -4,19 +4,23 @@ import java.util.TimeZone
 //These are the continued exercises from day 1
 object DayTwo {
   def main(args: Array[String]): Unit = {
+    val array2
+    for(x <- pattern2()){
+      print(x + ", ")
+    }
     for( x <- timeIDs()){
-      println(x)
+      for( y <- x){
+        print(y + ", ")
+      }
+      println()
     }
   }
-  def timeIDs(): Array[String] = {
-    TimeZone.getAvailableIDs().filter(sizeCheck(_))
+  def pattern2(numbers: any): Array[Int] = numbers match{
+    case numbers:Array[Int] => numbers.reverse
+    case numbers:List[Int] => numbers.toArray.reverse
+    case numbers: (x, y) => (y, x)
   }
-  def sizeCheck(ourString: String): Boolean = {
-    if(ourString.length() > 3){
-        true
-    }
-    else{
-        false
-    }
+  def timeIDs(): Array[Array[String]] = {
+    TimeZone.getAvailableIDs().filter(_.length() > 3).filter(_.contains('/')).mapInPlace(_.split('/')(1)).sliding(10, 10).toArray
   }
 }
